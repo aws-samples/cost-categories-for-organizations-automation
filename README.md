7f1cc4119ff069d08f19a312502f6f9f9000ed4a
Julien Follenfant <follenfa@amazon.com>
2023-06-20 18:33:05 -0400

    project publishing

--- README.md
+++ README.md
@@ -1,17 +1,89 @@
-## My Project
+# AWS Cost Categories for Organizations Automation
 
-TODO: Fill this README out!
 
-Be sure to:
+The **AWS Cost Categories for Organizations Automation** tool is designed to automate the process of creating AWS Cost Categories based on tags defined at accounts level in your AWS Organizations. It leverages AWS EventBridge to periodically check your AWS Organization account structure and dynamically create and update Cost Categories definitions for better cost allocation and management.
 
-* Change the title in this README
-* Edit your repository description on GitHub
 
-## Security
+## Table of Contents
+- [AWS Cost Categories for Organizations Automation](#aws-cost-categories-for-organizations-automation)
+  - [Table of Contents](#table-of-contents)
+  - [Introduction](#introduction)
+  - [Prerequisites](#prerequisites)
+  - [Installation and Configuration](#installation-and-configuration)
+  - [Usage](#usage)
+  - [Notes](#notes)
+  - [Roadmap](#roadmap)
+  - [License](#license)
 
-See [CONTRIBUTING](CONTRIBUTING.md#security-issue-notifications) for more information.
+## Introduction
 
+Managing costs within an AWS Organization can be challenging, especially when dealing with a large number of accounts and services. The AWS Cost Categories for Organization Automation tool simplifies this process by automatically generating cost categories based on the tags associated with each account in your AWS Organization.
+
+With this tool, you can define cost categories that align with your organization's cost allocation strategy and automatically update them as accounts and tags change within your AWS Organization. This automation helps you save time and ensure accurate cost attribution across your organization and allows you to visualize categories directly from the AWS Cost Explorer.
+
+## Prerequisites
+
+Before using the AWS Cost Categories for Organizations Automation tool, make sure you have the following prerequisites:
+
+- An AWS Organization Management account with consolidated billing enabled.
+- An AWS Organization structure where the account is the most granular billable entity.
+- At the AWS Organization level, tags associated directly with AWS Accounts that would be used for Cost Categories. 
+  - Note: Tags associated with Organization OUs are not currently used by this solution to build Cost Categories.
+- IAM credentials or Console access with appropriate permissions to create/update CloudFormation stacks that includes IAM Role creation.
+- No SCPs that prevents this solution IAM Role from using : 
+    - AWS Organizations.
+    - Cost Explorer categories.
+    - Lambda functions.
+    - CloudWatch Log Groups.
+    - Systems Manager Parameter Store. 
+    - See CloudFormation template `OrgCostCategoriesAutoRole` for the IAM Policies in details.
+  
+- Basic knowledge of AWS CloudFormation and its usage.
+- Basic knowledge of AWS Cost Explorer queries and filters.
+
+## Installation and Configuration
+
+To install and configure the AWS Cost Categories for Organizations Automation tool, follow these steps:
+
+1. Download the CloudFormation template provided in this GitHub repository, save it to your local machine. ( `AWS-Organization-Cost-Categories-automation-installer.yml` )
+2. Go to the CloudFormation Console
+3. Create a CloudFormation Stack from new resources, upload the CloudFormation template from your local machine.
+4. Choose a Stack name  
+   - eg: `OrgCostCategoriesAutomation`
+5. Provide the tag keys that should be considered for building the Cost Categories, be sure to use coma separated list 
+    -   eg: `CostCenter, ProjectEnv, ProjectID`
+    -   Keep in mind that AWS Tags are case sensitive.
+6. Choose the starting date by entering year and month for building categories, fields : `OrgCostCategoriesAutoStartMonth` and `OrgCostCategoriesAutoStartYear`.
+   - Important : AWS Cost Categories are built on Cost Explorer data, so you can go back in time up to 12 months.
+7. Leave `OrgCostCategoriesAutoSSMPathOrgAccountsDigest` and `OrgCostCategoriesAutoSSMPathOrgUnitsDigest` default values.
+8. Acknowledge that CloudFormation will create IAM resources, then Create.
+
+
+## Usage
+
+1. Following the installation steps, the CloudFormation stack should be in `CREATE_COMPLETE` state.
+2. A primer invoke is made from CloudFormation to build the Cost Categories.
+3. Once a day, the lambda runs and detect potential changes you made
+   - ie: add or remove tags and accounts in your AWS Organization.
+4. When adding or removing tags, you must :
+      -    First, add the new tags to the relevant AWS accounts that must be considered under new Cost Categories.
+      -    Update the CloudFormation stack parameters with the new tags or remove tags that are not in use anymore.
+      -    Note : This solution wont't delete any AWS Cost Categories, wether you update or delete the CloudFormation stack.
+
+## Notes
+Currently, this solution assumes that an AWS Account is the most granular element for you to do costs allocation, we plan to support other mechanisms for a more granular approach within multi tenants AWS accounts based on Cost Allocation Tags.
+
+There's no cost associated with the usage of AWS Cost Categories, however other services fees apply with this solution such as EventBridge, Lambda invocation and SSM Parameter Store, see :
+- https://aws.amazon.com/eventbridge/pricing/
+- https://aws.amazon.com/lambda/pricing/
+- https://aws.amazon.com/systems-manager/pricing/
+
+
+## Roadmap
+-   Support for AWS Cost Allocation Tags.
+-   Support for AWS Cost Categories by Organizational Units.
+-   Support for AWS Cost Categories deletion when tags removed.
+   
 ## License
-
-This library is licensed under the MIT-0 License. See the LICENSE file.
+This project is licensed under the MIT-0 License.
 
---
+++ aws_org_costcategories_auto.py
@@ -0,0 +1,270 @@
+"""AWS Cost Categories for Organization automation """
+from collections import defaultdict
+import json
+import hashlib
+import datetime
+import os
+import urllib
+import boto3
+from botocore.exceptions import ClientError
+
+
+CATEGORIES_TAGS = list(os.getenv("CCAT_TAGS_LIST").replace(" ","").split(","))
+CATEGORIES_START_DATE = datetime.datetime(int(os.getenv("CCAT_START_DATE_YEAR")),
+                                          int(os.getenv("CCAT_START_DATE_MONTH")),
+                                          1)
+
+SSM_PARAM_ORG_ACC_DIGEST = os.getenv("CCAT_SSM_PATH_ORG_ACC")
+SSM_PARAM_ORG_OUS_DIGEST = os.getenv("CCAT_SSM_PATH_ORG_OUS")
+DEBUG_FUNC = False
+
+def ce_list_cost_categories(cost_categories_arns):
+    """
+    List cost categories managed by this script based on owner tag"""
+
+    client = boto3.client('ce')
+    ccat_definitions = client.list_cost_category_definitions()
+    for ccat in ccat_definitions['CostCategoryReferences']:
+        ccat_tags = client.list_tags_for_resource(ResourceArn=ccat['CostCategoryArn'])
+        for tag in ccat_tags['ResourceTags']:
+            if tag['Key'] == "aws-finops-managed" and tag['Value'] == "true":
+                cost_categories_arns[ccat['Name']] = ccat['CostCategoryArn']
+
+
+def org_list_accounts(org_accounts):
+    """
+    List all accounts ids in current AWS Organization"""
+
+    client = boto3.client('organizations')
+    paginator = client.get_paginator('list_accounts')
+    iterator  = paginator.paginate()
+    for page in iterator:
+        for account in page['Accounts']:
+            org_accounts.append(account['Id'])
+
+
+def recursive_ou_search(parent, org_ous):
+    """
+    Recursively search OU parent"""
+
+    client = boto3.client('organizations')
+    paginator = client.get_paginator('list_organizational_units_for_parent')
+    iterator  = paginator.paginate(ParentId=parent)
+    for page in iterator:
+        for ou_name in page['OrganizationalUnits']:
+            org_ous.add(ou_name['Id'])
+            recursive_ou_search(ou_name['Id'], org_ous)
+
+
+def org_list_ous(org_ous):
+    """
+    List all Organizational Units in the current AWS Organization"""
+
+    org_roots = set()
+    client = boto3.client('organizations')
+
+    paginator = client.get_paginator('list_roots')
+    iterator  = paginator.paginate()
+    for page in iterator:
+        for root in page['Roots']:
+            org_roots.add(root['Id'])
+
+    recursive_ou_search(org_roots.pop(), org_ous)
+
+
+def org_fetch_tags_for_account(account, tags_tree):
+    """
+    List all tags for an AWS account id"""
+
+    client = boto3.client('organizations')
+    paginator = client.get_paginator('list_tags_for_resource')
+    iterator  = paginator.paginate(ResourceId=account)
+    for page in iterator:
+        for tags in page['Tags']:
+            if tags['Key'] in CATEGORIES_TAGS:
+                tags_tree[account].append(tags)
+
+
+def org_fetch_tags_for_ou(org_unit, tags_tree):
+    """
+    List all tags for an AWS Organizational Unit id"""
+
+    client = boto3.client('organizations')
+    paginator = client.get_paginator('list_tags_for_resource')
+    iterator  = paginator.paginate(ResourceId=org_unit)
+    for page in iterator:
+        for tags in page['Tags']:
+            tags_tree[org_unit].append(tags)
+
+
+def ssm_save_digest(param_path, digest):
+    """
+    Save AWS Organization objects digest"""
+
+    client = boto3.client('ssm')
+    client.put_parameter(
+        Name=param_path,
+        Description='AWS Organization and Org Units MD5 Digest',
+        Value=digest,
+        Type='String',
+        Overwrite=True,
+        Tier='Standard',
+        DataType='text'
+    )
+
+def ssm_get_digest(param_path):
+    """
+    Get AWS Organization objects digest"""
+
+    client = boto3.client('ssm')
+    try:
+        response = client.get_parameter(Name=param_path)
+    except ClientError as err:
+        if err.response['Error']['Code'] == 'ParameterNotFound':
+            return '-1'
+
+    return response['Parameter']['Value']
+
+    
+def ce_build_cost_category_definitions(cost_categories_arns, tags_tree):
+    """
+    Create cost category based on selected tag / values"""
+
+    cc_defs = defaultdict(list)
+    cost_categories_map = defaultdict(lambda: defaultdict(list))
+    ccat_rules = defaultdict(list)
+    ccat_rule = {
+        "Value": "", 
+        "Rule": {
+            "Dimensions": {
+                "Key": "LINKED_ACCOUNT",
+                "Values": [],
+                "MatchOptions": ["EQUALS"]
+            }
+        },
+        "Type": "REGULAR"
+    }
+
+    for account, tags_map in tags_tree.items():
+        for tag in tags_map:
+            cost_categories_map[tag['Key']][tag['Value']].append(account)
+
+    for provided_tag, _ in cost_categories_map.items():
+        ccat_rules[provided_tag] = ccat_rule.copy()
+        for key in cost_categories_map[provided_tag].items():
+            ccat_rules[provided_tag]['Value'] = key[0]
+            ccat_rules[provided_tag]['Rule']['Dimensions']['Values'] = key[1]
+            cc_defs[provided_tag].append(json.dumps(ccat_rules[provided_tag]))
+
+
+    client = boto3.client('ce')
+
+    for cc_name, cc_def  in cc_defs.items():
+        rules = []
+        for rule in cc_def:
+            rules.append(json.loads(rule))
+        if cc_name in cost_categories_arns:
+            print(f"Cost Category \"{cc_name}\" exists and managed by this script. Updating...")
+            client.update_cost_category_definition(
+                CostCategoryArn=cost_categories_arns[cc_name],
+                EffectiveStart=CATEGORIES_START_DATE.strftime("%Y-%m-01T00:00:00Z"),
+                RuleVersion='CostCategoryExpression.v1',
+                Rules=json.loads(json.dumps(rules)),
+            )
+        else:
+            print(f"Cost Category \"{cc_name}\" detected from tags. Creating...")
+            client.create_cost_category_definition(
+                Name=cc_name,
+                EffectiveStart=CATEGORIES_START_DATE.strftime("%Y-%m-01T00:00:00Z"),
+                RuleVersion='CostCategoryExpression.v1',
+                Rules=json.loads(json.dumps(rules)),
+                ResourceTags=[
+                    {
+                        'Key': 'aws-finops-managed',
+                        'Value': 'true'
+                    }]
+            )
+
+
+def hash_dict(obj):
+    """
+    Helper function to hash dict results"""
+
+    return hashlib.md5(json.dumps(obj, sort_keys=True).encode("utf-8")).hexdigest()
+
+
+def send_response(event, context, response):
+    """
+    Send a response to CloudFormation to handle the custom resource lifecycle"""
+
+    response_body = {
+        'Status': response,
+        'Reason': 'See details in CloudWatch Log Stream: ' + \
+            context.log_stream_name,
+        'PhysicalResourceId': context.log_stream_name,
+        'StackId': event['StackId'],
+        'RequestId': event['RequestId'],
+        'LogicalResourceId': event['LogicalResourceId'],
+    }
+    print('RESPONSE BODY: \n' + json.dumps(response_body))
+    data = json.dumps(response_body).encode('utf-8')
+    req = urllib.request.Request(
+        event['ResponseURL'],
+        data,
+        headers={'Content-Length': len(data), 'Content-Type': ''})
+    req.get_method = lambda: 'PUT'
+    try:
+        with urllib.request.urlopen(req) as resp:
+            print(f'response.status: {resp.status}, ' +
+                  f'response.reason: {resp.reason}')
+            print('response from cfn: ' + resp.read().decode('utf-8'))
+    except urllib.error.URLError:
+        raise Exception('Received non-200 response while sending response to AWS CloudFormation')
+    return True
+
+
+def lambda_handler(event, context):
+    """Lambda function handler"""
+
+    if event['RequestType'] == 'Create' or event['RequestType'] == 'Update':
+        send_response(event, context, "SUCCESS")
+
+    if event['RequestType'] == 'Delete':
+        send_response(event, context, "SUCCESS")
+        return # skip function execution
+        
+    tags_tree = defaultdict(list)
+    cost_categories_arns = {}
+    org_accounts = []
+    org_ous = set()
+
+    # Debug Lambda function event and context
+    if DEBUG_FUNC:
+        print(f"{event}\n{context}")
+
+    # Org units
+    org_list_ous(org_ous)
+
+    # Org accounts
+    org_list_accounts(org_accounts)
+    for acc_id in org_accounts:
+        org_fetch_tags_for_account(acc_id, tags_tree)
+
+    org_units = list(org_ous)
+    org_units.sort()
+
+    if (
+        hash_dict(tags_tree) == ssm_get_digest(SSM_PARAM_ORG_ACC_DIGEST) and
+        hash_dict(org_units) == ssm_get_digest(SSM_PARAM_ORG_OUS_DIGEST)
+        ):
+        print("Nothing to do, no change detected on AWS Organizations Accounts and OUs.")
+    else:
+        ce_list_cost_categories(cost_categories_arns)
+        ce_build_cost_category_definitions(cost_categories_arns, tags_tree)
+
+        ssm_save_digest(SSM_PARAM_ORG_ACC_DIGEST, hash_dict(tags_tree))
+        ssm_save_digest(SSM_PARAM_ORG_OUS_DIGEST, hash_dict(org_units))
+
+
+
+lambda_handler(event="", context="")
